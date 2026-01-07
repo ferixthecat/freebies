6219c24de24bb528c5d5b7ac31d87b8c9e85d2a4
Felix Tse <felixtse283416@gmail.com>
2026-01-07 10:54:35 -0500

    basic hooks, services, dummy

--- data/categories.ts
+++ data/categories.ts
@@ -5,3 +5,48 @@ export interface Category {
   image: any;
   backgroundColor: string;
 }
+
+export const categories: Category[] = [
+  {
+    id: "food",
+    name: "Food",
+    placesCount: 12,
+    image: require("../assets/images/categories/food.png"),
+    backgroundColor: "#E8DCD9",
+  },
+  {
+    id: "drinks",
+    name: "Drinks",
+    placesCount: 8,
+    image: require("../assets/images/categories/drinks.png"),
+    backgroundColor: "#4ECDC4",
+  },
+  {
+    id: "desserts",
+    name: "Desserts",
+    placesCount: 6,
+    image: require("@/assets/categories/desserts.png"),
+    backgroundColor: "#FFB6C1",
+  },
+  {
+    id: "beauty",
+    name: "Beauty",
+    placesCount: 5,
+    image: require("@/assets/categories/beauty.png"),
+    backgroundColor: "#DDA0DD",
+  },
+  {
+    id: "retail",
+    name: "Retail",
+    placesCount: 7,
+    image: require("@/assets/categories/retail.png"),
+    backgroundColor: "#87CEEB",
+  },
+  {
+    id: "entertainment",
+    name: "Entertainment",
+    placesCount: 4,
+    image: require("@/assets/categories/entertainment.png"),
+    backgroundColor: "#FFD700",
+  },
+];
